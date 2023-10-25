942d2607a04b9bfa9c8c64ca7d793428ca359c30
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2023-10-25 08:20:24 +0530

    Time: 41 ms (35.95%) | Memory: 16.3 MB (17.91%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.py
@@ -0,0 +1,22 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 0:
+            return 0
+
+        def bit_place(num, left, right, k1):
+            m = (left + right) // 2
+            if k1 == n:
+                return num
+            if left <= k <= m:
+                if num[0] == '0':
+                    return bit_place("0", left, m, k1 + 1)
+                else:
+                    return  bit_place("1", left, m, k1 + 1)
+
+            else:
+                if num[-1] == "0":
+                    return bit_place("1", m + 1, right, k1 + 1)
+                else:
+                    return bit_place("0", m + 1, right, k1 + 1)
+
+        return int(bit_place("0", 1, 2 ** (n - 1), 0))
