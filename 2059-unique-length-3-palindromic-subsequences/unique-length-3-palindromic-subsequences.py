884408464489491ba9110a0419af6e6953e4b491
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2023-11-14 17:11:29 +0530

    Time: 672 ms (50.66%) | Memory: 17.3 MB (43.97%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,27 +1,15 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        char_set=set(s)
-
-
-
-        def index_last(s,i):
-            for ind in range(len(s)-1,-1,-1):
-                print(s[ind],ind)
-                if s[ind]==i :
-                    return ind
-
+        letters = set(s)
+        ans = 0
         
-
-        ans=0
-        for i in char_set :
-            l=s.index(i)
-            r=index_last(s,i)  
-            if r>(l+1) :         #       a.....a   finding all the distinct characters between them including a as well
-                char=set()
-                for ch in range(l+1,r):
-                    char.add(s[ch])
-                ans+=len(char)
+        for letter in letters:
+            i, j = s.index(letter), s.rindex(letter)
+            between = set()
+            
+            for k in range(i + 1, j):
+                between.add(s[k])
+            
+            ans += len(between)
 
         return ans
-
-        
