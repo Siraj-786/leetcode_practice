9b5a7de827962db9f65dfeb2081f4f028d558470
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2023-11-22 17:20:56 +0530

    Time: 784 ms (67.10%) | Memory: 41.3 MB (25.18%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -1,12 +1,16 @@
-from typing import List
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        l1=[]
-        for i in range(len(nums)):
-            for j in range(len(nums[i])):
-                l1.append((i+j,i,j,nums[i][j]))
-        sorted_data = sorted(l1, key=lambda x:(x[0],-x[1]))
-        ans=[]
-        for i in sorted_data:
-            ans.append(i[3])
+        groups = defaultdict(list)
+        for row in range(len(nums) - 1, -1, -1):
+            for col in range(len(nums[row])):
+                diagonal = row + col
+                groups[diagonal].append(nums[row][col])
+                
+        ans = []
+        curr = 0
+        
+        while curr in groups:
+            ans.extend(groups[curr])
+            curr += 1
+
         return ans
