a44915c320540ae32f5ff0556e481430edfd3299
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2023-11-22 17:15:54 +0530

    Time: 831 ms (23.07%) | Memory: 47 MB (7.50%) - LeetSync

---
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        l1=[]
+        for i in range(len(nums)):
+            for j in range(len(nums[i])):
+                l1.append((i+j,i,j))
+        sorted_data = sorted(l1, key=lambda x:(x[0],-x[1]))
+        ans=[]
+        for i in sorted_data:
+            ans.append(nums[i[1]][i[2]])
+        return ans
