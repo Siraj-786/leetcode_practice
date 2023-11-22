9795c0de81bc70d2c3015186f61cf75191d1f04f
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2023-11-22 17:17:10 +0530

    Time: 817 ms (29.98%) | Memory: 48.4 MB (6.70%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.py
@@ -4,9 +4,9 @@ class Solution:
         l1=[]
         for i in range(len(nums)):
             for j in range(len(nums[i])):
-                l1.append((i+j,i,j))
+                l1.append((i+j,i,j,nums[i][j]))
         sorted_data = sorted(l1, key=lambda x:(x[0],-x[1]))
         ans=[]
         for i in sorted_data:
-            ans.append(nums[i[1]][i[2]])
+            ans.append(i[3])
         return ans
