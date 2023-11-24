bcadfdd4c7031bf3e462f41fe53871ffd39d2b10
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2023-11-24 11:18:32 +0530

    Time: 571 ms (12.02%) | Memory: 28.9 MB (12.68%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -2,7 +2,6 @@ class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         ans=0
         piles.sort()
-        print(piles)
         for i in range(0,len(piles)//3):
             print(piles[(-2)*(i+1)])
             ans+=piles[(-2)*(i+1)]
