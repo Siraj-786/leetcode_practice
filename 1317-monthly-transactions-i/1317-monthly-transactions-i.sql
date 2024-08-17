1177649cc63bf67604f3117f162c246e3151181a
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-08-17 19:17:34 +0530

    Time: 562 ms (69.05%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -3,7 +3,7 @@ select
 left(trans_date,7) as month 
 ,country,
 count(id) as trans_count,
-sum(if(state='approved',1,0)) as approved_count,
+sum(state='approved') as approved_count,
 sum(amount) as trans_total_amount,
 sum(if(state='approved',1,0)*amount) as approved_total_amount
 from Transactions
