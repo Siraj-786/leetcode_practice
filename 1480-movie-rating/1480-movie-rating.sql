74100671ec0baa66e9c28a8392ef1a2311a087a6
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-08-26 20:36:56 +0530

    Time: 960 ms (97.87%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,33 +1,32 @@
 # Write your MySQL query statement below
+ # done by me @siraj
 select min(t1.name) as results   from (
-select u.name,count(distinct movie_id) as no_of from 
-    MovieRating as m 
-    left join 
-    Users as u 
-    on m.user_id=u.user_id
-    group by m.user_id
-    order by no_of) as t1
-    where t1.no_of=(select max(no_of) from (select u.name,count(distinct movie_id) as no_of from 
-    MovieRating as m 
-    left join 
-    Users as u 
-    on m.user_id=u.user_id
-    group by m.user_id
-    order by no_of) as t1)
-
+    select u.name,count(distinct movie_id) as no_of from 
+        MovieRating as m 
+        left join 
+        Users as u 
+        on m.user_id=u.user_id
+        group by m.user_id
+        order by no_of) as t1
+        where t1.no_of=(select max(no_of) from (select u.name,count(distinct movie_id) as no_of from 
+                        MovieRating as m 
+                        left join 
+                        Users as u 
+                        on m.user_id=u.user_id
+                        group by m.user_id
+                        order by no_of) as t1)
 union  all
-
 select t.t1 as results from 
 
-(select avg(m1.rating) as av,m2.title as t1  from 
-    MovieRating as m1
-    left join 
-    Movies as m2 
-    on m1.movie_id=m2.movie_id 
-    where m1.created_at between '2020-02-01' and '2020-02-29'
-    group by m1.movie_id
-    order by av desc,t1 
-    limit 1) as t;
+    (select avg(m1.rating) as av,m2.title as t1  from 
+        MovieRating as m1
+        left join 
+        Movies as m2 
+        on m1.movie_id=m2.movie_id 
+        where m1.created_at between '2020-02-01' and '2020-02-29'
+        group by m1.movie_id
+        order by av desc,t1 
+        limit 1) as t;
 
 
 
