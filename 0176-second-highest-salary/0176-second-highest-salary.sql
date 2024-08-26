592d52fd7db93dbaa96edcba7bfd24bf14477d07
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-08-26 16:49:53 +0530

    Time: 218 ms (92.27%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,14 +1,7 @@
-select case 
-    when (select max(salary) from Employee)=salary then  null
-    else salary
-    end as SecondHighestSalary
-from Employee
-order by SecondHighestSalary desc
-limit 1;
 
--- SELECT   
---     CASE   
---         WHEN salary = (SELECT MAX(salary) FROM Employee) THEN salary - 10000  
---         ELSE salary   
---     END AS new_salary  
--- FROM Employee;
+-- wha the hell way to handle null in the question
+select (
+select distinct salary 
+from Employee
+order by salary desc
+limit 1 offset 1) as SecondHighestSalary;
