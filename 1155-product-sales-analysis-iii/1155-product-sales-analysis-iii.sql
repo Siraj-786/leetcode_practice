c4dae23f9d5961ed33b228b9b5b819ffc660654c
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-08-26 08:50:19 +0530

    Time: 1002 ms (64.16%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -2,5 +2,4 @@ select product_id,
     year as first_year,
     quantity ,price 
 from Sales
-where (product_id,year) in 
-    (select product_id,min(year) as year from Sales group by product_id);
+where (product_id,year) in ( select product_id,min(year) from Sales group by product_id);
