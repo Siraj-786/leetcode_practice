b8b5bb29f629b2bf72a57c8d1075e444b3dad26b
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-09-20 19:57:24 +0530

    Time: 410 ms (31.56%), Space: 17.1 MB (41.65%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,15 +1,8 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        length = len(s)
-
-        #wtf why is it accepting and that too better than 95% sol though its n^2 solution 
-        
-
-        reversed_string = s[::-1]  # Reverse the string
-
-        # Iterate through the string to find the longest palindromic prefix
-        for i in range(length):
-            if s[: length - i] == reversed_string[i:]:
-
-                return reversed_string[:i] + s
-        return ""
+        for i in range(len(s)):
+            # print(s[len(s)-i::-1])
+            if s[0:len(s)-i]==s[0:len(s)-i][::-1] :
+                # print(s[0:len(s)-i])
+                return s[len(s)-(i):][::-1]+s
+        return s
