01cbf4c43cb8030999d2bd352d305cc162251356
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-09-25 10:52:50 +0530

    Time: 1328 ms (92.07%), Space: 228.3 MB (90.69%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,33 @@
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        trie=defaultdict()
+        def insert(word):
+            curr=trie 
+            for c in word :
+                if c in curr :
+                    p=curr[c]
+                    p["*"]+=1
+                    curr=curr[c]
+                else :
+                    curr[c]=defaultdict()
+                    curr=curr[c]
+                    curr["*"]=1
+        for ele in words :
+            insert(ele)
+        ans=[]
+        def get_ans(word):
+            s=0
+            curr=trie
+            for c in word :
+                curr=curr[c]
+                s+=curr["*"]
+            return s 
+        for ele in words :
+            ans.append(get_ans(ele))
+        return ans 
+
+
+
+
+        
+        
