60f1fab8a163bee1f4626ef81871ee4a3bb920d0
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-09-29 10:08:23 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,6 +1,23 @@
 class Solution:
+    p = []
+    s = set()
+
     def getPermutation(self, n: int, k: int) -> str:
-        p=list(permutations([i+1 for i in range(n)]))
-        # print(p)
-        return "".join(str(i) for i in p[k-1])
-        
+        self.p.clear()  # Clear previous results
+        self.s.clear()  # Clear previous set
+
+        def recurse(lis):
+            if len(lis) == n:
+                self.p.append(lis.copy())
+                return
+
+            for i in range(1, n + 1):
+                if i not in self.s:
+                    lis.append(i)
+                    self.s.add(i)
+                    recurse(lis)
+                    lis.pop(-1)
+                    self.s.remove(i)
+
+        recurse([])
+        return "".join(str(i) for i in self.p[k - 1])
