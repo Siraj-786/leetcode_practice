42eb8a50574cb3bf02e20af2392cae0c0d73c281
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-10-04 20:34:29 +0530

    Time: 5875 ms (5.02%), Space: 30.9 MB (41.54%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        prices.append(0)
+        prices.append(0)
+        pre_min_a=[prices[0]]
+        for i in range(1,len(prices)):
+            pre_min_a.append(min(pre_min_a[-1],prices[i]))
+        # print(pre_min_a)
+        diffs=[]
+        for i in range(len(prices)):
+            diffs.append(prices[i]-pre_min_a[i])
+        new_diffs=[]
+        for i in range(len(diffs)):
+            if new_diffs :
+                new_diffs.append(max(new_diffs[-1],diffs[i]))
+            else :
+                new_diffs.append(diffs[i])
+        diffs=new_diffs
+        suff_max=[prices[-1]]
+        for i in range(len(prices)-2,-1,-1):
+            suff_max.insert(0,max(suff_max[0],prices[i]))
+        max_ans=0
+        for i in range(1,len(prices)):
+            max_ans=max(max_ans,diffs[i-1]+suff_max[i]-prices[i])
+        return max_ans
+
+
+        
