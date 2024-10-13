21c434923c97c93979a62a092ac4a298b6113314
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-10-13 12:01:43 +0530

    Time: 259 ms (11.65%), Space: 25.9 MB (5.75%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -1,36 +1,44 @@
-from collections import defaultdict
-from typing import List
-
 class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
-        num = []
+        num=[]
 
-        k = 1
+        k=1
         for ele in nums:
-            for c in ele:
-                num.append((c, k))
-            k += 1
-
+            for c in ele :
+                num.append((c,k))
+            k+=1 
         num.sort()
-        d = defaultdict(int)
-        min_len = float("inf")
-        l, r = float("-inf"), float("inf")
-        q = []
-
-        for ele in num:
-            d[ele[1]] += 1
+        d=defaultdict(int)
+        min_len=float("inf")
+        l,r=float("-inf"),float("inf")
+        q=[]
+        for ele in num :
+            d[ele[1]]+=1
             q.append(ele)
-            
-            while len(d) == len(nums):
-                t = q[-1][0] - q[0][0]
-                if t < min_len:
-                    min_len = t
-                    l = q[0][0]
-                    r = q[-1][0]
-                
-                p = q.pop(0)
-                d[p[1]] -= 1
-                if d[p[1]] == 0:
-                    del d[p[1]]
-                
-        return [l, r]
+            if len(d)==len(nums):
+                t=q[-1][0]-q[0][0]
+                if t<min_len :
+                    min_len=t 
+                    l=q[0][0]
+                    r=q[-1][0]
+                while len(d)==len(nums) :
+                    p=q.pop(0)
+                    d[p[1]]-=1
+                    if d[p[1]]==0 :
+                        del d[p[1]]
+                    if len(d)==len(nums) :
+                        t=q[-1][0]-q[0][0]
+                        if t<min_len :
+                            min_len=t 
+                            l=q[0][0]
+                            r=q[-1][0]
+        return [l,r]
+
+
+
+
+
+
+
+        
+        
