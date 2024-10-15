55f37a224834e7414213d3b9e1fde4dad9f57101
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-10-15 14:57:20 +0530

    Time: 111 ms (40.60%) | Memory: 17.7 MB (20.90%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -3,13 +3,12 @@ class Solution:
         ans=0
         o=s.count("1")
         a1=0
-        k=len(s)-1
-        for i in range(o):
-            a1+=k
-            k-=1 
         a2=0
+        k=len(s)-1
         for i in range(len(s)):
-            if s[i]=="1" :
+            if s[i]=="1":
+                a1+=k
+                k-=1 
                 a2+=i 
         return a1-a2
 
