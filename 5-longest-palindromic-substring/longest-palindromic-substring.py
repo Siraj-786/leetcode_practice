1727a0f8630cbb600f8e7dd4f5640f3dc216d8aa
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-10-15 17:02:08 +0530

    Time: 358 ms (51.31%) | Memory: 16.5 MB (77.65%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,46 +1,41 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        # two pointers kind off 
-        mini_len_found=1
-        ans=s[0]
-        def check_odd(p):
-            a,b=p,p
-            
-            while a>0 and b<len(s)-1:
-                a-=1 
-                b+=1
-                if s[a]!=s[b] :
-                    a+=1 
-                    b-=1 
-                    break
-            return s[a:b+1]
-        def check_even(a,b):
-            if s[a]!=s[b] :
-                return s[a]
-            
-            while a>0 and b<len(s)-1 :
-                a-=1
-                b+=1 
-                if s[a]!=s[b] :
-                    a+=1
-                    b-=1 
-                    break
-            return s[a:b+1]
+        # odd
+        def check_max(i):
+            j=i 
+            k=1
+            while j-k>=0 and j+k<len(s) :
+                if s[j-k]==s[j+k] :
+                    k+=1
+                else :
+                    break 
+            return 1+2*(k-1)
+        ans=""
+        max_len=0
+        for i in range(len(s)):
+            r=check_max(i)
+            if r>max_len :
+                max_len=r 
+                ans=s[i-r//2:i+1+r//2]
+        
+        def check_max1(i):
+            j=i 
+            k=1
+            while j-k>=0 and j+k+1<len(s) :
+                if s[j-k]==s[j+k+1] :
+                    k+=1
+                else :
+                    break 
+            return 2*(k)
         for i in range(len(s)-1):
-            # for odd len 
-            centre=i 
-            ch_odd=check_odd(i)
-            if len(ch_odd)>mini_len_found :
-                mini_len_found=len(ch_odd)
-                ans=ch_odd
-            ch_even=check_even(i,i+1)
-            print(ch_even)
-            if len(ch_even)>mini_len_found :
-                mini_len_found=len(ch_even)
-                ans=ch_even 
+            if s[i]==s[i+1] :
+                r=check_max1(i)
+                if r>max_len :
+                    max_len=r 
+                    ans=s[i-(r-2)//2:i+1+r//2]
         return ans
 
 
+        
 
-            
-
+        
