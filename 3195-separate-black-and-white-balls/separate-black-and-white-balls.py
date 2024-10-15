d03fbe37e827bfe99385fcdc576e12e89c1aa09a
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-10-15 14:55:12 +0530

    Time: 117 ms (31.64%) | Memory: 21.3 MB (5.07%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -1,13 +1,17 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        z = s.count("0")
-        if (z * "0" + (len(s) - z) * "1") == s:
-            return 0
-        one_index = []
-        for i in range(len(s)):
-            if s[i] == "1":
-                one_index.append(i)
         ans=0
-        for i in range(z,len(s)):
-            ans+=(i-one_index[i-z])
-        return ans
+        o=s.count("1")
+        a1=[]
+        k=len(s)-1
+        for i in range(o):
+            a1.append(k)
+            k-=1 
+        a2=[]
+        for i in range(len(s)):
+            if s[i]=="1" :
+                a2.append(i)
+        return sum(a1)-sum(a2)
+
+
+       
