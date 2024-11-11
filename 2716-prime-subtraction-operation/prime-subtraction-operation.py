a8d78461e221bf706262a8a154dd5ca252ed60d8
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-11-11 08:50:08 +0530

    Time: 285 ms (8.94%) | Memory: 17 MB (5.34%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.py
@@ -0,0 +1,28 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+
+        def sieve(n):
+            p=[1]*(n+2)
+            for i in range(2,n):
+                k=2
+                while k*i<=n :
+                    p[k*i]=0 
+                    k+=1 
+            return [i for i in range(n+2) if p[i]==1][2:] 
+        s=sieve(1000)
+        # print(s)
+        # return False
+
+        for i in range(len(nums)-2,-1,-1):
+            while nums[i+1]<=nums[i] :
+                for k in range(len(s)) :
+                    if nums[i+1]>nums[i]-s[k] :
+                        nums[i]=nums[i]-s[k] 
+                        break 
+                break 
+        print(nums)
+        if nums[0]<=0 :
+            return False 
+        return nums==sorted(nums) and len(set(nums))==len(nums)
+
+        
