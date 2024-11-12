a2397c0befa501b2e04c7954c5aefae5a40a221c
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-11-12 20:20:15 +0530

    Time: 111 ms (93.90%) | Memory: 66.4 MB (60.22%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+
+        d=defaultdict(int)
+        for ele in items :
+            d[ele[0]]=max(d[ele[0]],ele[1])
+        s=list(d.keys()) 
+        s.sort()
+        val=[]
+        for i in  s :
+            val.append(d[i])
+        prefix=[val[0]]
+        for i in range(1,len(val)):
+            prefix.append(max(prefix[-1],val[i]))
+        print(prefix)
+        ans=[]
+        for i in range(len(queries)):
+            ind=bisect.bisect_right(s,queries[i])
+            if ind==0 :
+                ans.append(0)
+            else :
+                ans.append(prefix[ind-1])
+        return ans 
+        
