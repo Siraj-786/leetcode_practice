8c17979b1a8657941f6458e7b40cbcee5d8c79a3
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2024-12-04 17:57:07 +0530

    Time: 560 ms (97.59%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,4 +1,6 @@
-select round(sum(order_date = customer_pref_delivery_date)/count(*) * 100, 2) as immediate_percentage
+select 
+round(sum(order_date = customer_pref_delivery_date)/count(*) * 100, 2) 
+as immediate_percentage
 from
 (
 select *
