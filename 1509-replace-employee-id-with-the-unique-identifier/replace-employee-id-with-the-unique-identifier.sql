6a45906834ca62f125baf41214875429667535e4
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2025-02-08 10:18:13 +0530

    Time: 1202 ms (92.35%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-select unique_id as unique_id,name from 
-employees e
-left join  EmployeeUNI u 
-on u.id = e.id
+select 
+u.unique_id ,name
+from employees e 
+left join EmployeeUNI u 
+on e.id=u.id
