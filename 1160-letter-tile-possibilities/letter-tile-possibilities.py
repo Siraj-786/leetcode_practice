ba14ffb57d2707675704838ffda9f8a74d648408
shaik siraj <102654065+Siraj-786@users.noreply.github.com>
2025-02-17 10:01:11 +0530

    Time: 159 ms (8.27%) | Memory: 27 MB (6.91%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,10 +1,22 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        s=set()
+        ss=set()
+        def permutations(s,i,curr,pos,need):
+            if len(curr)==need :
+                ss.add(curr)
+                return
+            else :
+                for i in range(len(s)):
+                    if i not in pos :
+                        curr+=s[i]
+                        permutations(s,i,curr,pos+[i],need)
+                        curr=curr[0:-1]
+        # permutations("abc",0,"",[],2)
+        print(ss)
+
         for i in range(1,len(tiles)+1):
-            for ele in permutations(tiles,i):
-                s.add(ele)
-        return len(s)
+            permutations(tiles,0,"",[],i)
+        return len(ss)
         
                   
         
